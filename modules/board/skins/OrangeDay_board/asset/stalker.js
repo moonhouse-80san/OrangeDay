827fe9e5f4a608d9daa9690b7a72f65626655741
moonhouse-80san <mbg1346@gmail.com>
2025-01-17 17:28:46 +0900

    오렌지데이 보드 V2.0.3 수정

--- modules/board/skins/OrangeDay_board/asset/stalker.js
+++ modules/board/skins/OrangeDay_board/asset/stalker.js
@@ -6,14 +6,6 @@ $(window).scroll(function() {
   }, 500); // Reduced animation time for better responsiveness
 });
 
-// Scroll to bottom function 
-function scrollToBottom() {
-  window.scrollTo({
-	top: document.documentElement.scrollHeight || document.body.scrollHeight,
-	behavior: 'smooth'
-  });
-}
-
 //페이지 하단으로 보내버려
 function pageScroll(){
 	window.scroll(0, document.documentElement.scrollHeight || document.body.scrollHeight); 
